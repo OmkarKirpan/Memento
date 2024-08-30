89eafadca936ac21985fe62696bf147e2d7dd9d2
Omkar Kirpan <okirpan@gmail.com>
2024-08-31 01:00:51 +0530

    feat: :art: add audio, update bg and set wins to localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import "./App.css";
 import { useState, useEffect } from "react";
 import Card from "./components/Card.jsx";
 import Header from "./components/Header.jsx";
+import useSound from "use-sound";
+import cardSound from "./card.mp3";
+import matchCardSound from "./cardMatch.mp3";
 
 function App() {
   const [wins, setWins] = useState(0);
@@ -10,8 +13,42 @@ function App() {
   const [pickOne, setPickOne] = useState(null);
   const [pickTwo, setPickTwo] = useState(null);
   const [disabled, setDisabled] = useState(false);
+  const [volume, setVolume] = useState(0.75);
+  const [play] = useSound(cardSound, {
+    // playbackRate,
+    // `interrupt` ensures that if the sound starts again before it's
+    // ended, it will truncate it. Otherwise, the sound can overlap.
+    interrupt: true,
+    volume,
+  });
+  const [playMatch] = useSound(matchCardSound);
+
+  // Array of gradient colors
+  const gradients = [
+    // "linear-gradient(to right, #ff7e5f, #feb47b)", // Warm gradient
+    // "linear-gradient(to right, #6a11cb, #2575fc)", // Purple to blue gradient
+    // "linear-gradient(to right, #00b4db, #0083b0)", // Cyan to blue gradient
+    // "linear-gradient(to right, #ff4b1f, #1fddff)", // Red to light blue gradient
+    // "linear-gradient(to right, #fbc2eb, #a6c1ee)", // Pink to light blue gradient
+    "linear-gradient(to top, #a18cd1 0%, #fbc2eb 100%)",
+    "linear-gradient(to right, #ff8177 0%, #ff867a 0%, #ff8c7f 21%, #f99185 52%, #cf556c 78%, #b12a5b 100%)",
+    "linear-gradient(to right, #4facfe 0%, #00f2fe 100%)",
+    "linear-gradient(to top, #fddb92 0%, #d1fdff 100%)",
+    "linear-gradient(to right, #f78ca0 0%, #f9748f 19%, #fd868c 60%, #fe9a8b 100%)",
+    "linear-gradient(to top, #f43b47 0%, #453a94 100%)",
+  ];
+
+  // State to keep track of the current gradient index
+  const [currentGradient, setCurrentGradient] = useState(0);
+
+  // Function to shuffle the background gradient
+  const shuffleGradient = () => {
+    setCurrentGradient((prevGradient) => (prevGradient + 1) % gradients.length);
+  };
 
   const handleClick = (card) => {
+    setVolume(Math.random());
+    play();
     if (!disabled) {
       pickOne ? setPickTwo(card) : setPickOne(card);
     }
@@ -24,10 +61,17 @@ function App() {
   };
 
   const handleNewGame = () => {
-    setWins(0);
+    // setWins(0);
     handleTurn();
     setCards(shuffle);
   };
+
+  useEffect(() => {
+    const localWins = localStorage.getItem("wins");
+    if (localWins) {
+      setWins(Number(localWins));
+    }
+  }, []);
 
   useEffect(() => {
     let pickTimer;
@@ -36,6 +80,9 @@ function App() {
         setCards((prevCards) => {
           return prevCards.map((card) => {
             if (card.image === pickOne.image) {
+              shuffleGradient();
+              document.body.style.background = gradients[currentGradient];
+              playMatch();
               return { ...card, matched: true };
             } else {
               return card;
@@ -54,7 +101,7 @@ function App() {
     return () => {
       clearTimeout(pickTimer);
     };
-  }, [cards, pickOne, pickTwo]);
+  }, [cards, pickOne, pickTwo, playMatch]);
 
   useEffect(() => {
     const checkWin = cards.filter((card) => !card.matched);
@@ -62,7 +109,10 @@ function App() {
     if (cards.length && checkWin.length < 1) {
       console.log("You win!");
       setWins(wins + 1);
+      localStorage.setItem("wins", Number(wins) + 1);
       handleTurn();
+      shuffleGradient();
+      document.body.style.background = gradients[currentGradient];
       setCards(shuffle);
     }
   }, [cards, wins]);
