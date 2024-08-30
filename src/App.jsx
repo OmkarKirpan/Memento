94f570d9b12d20356d6dfe9268e8de614d5a5491
Omkar Kirpan <okirpan@gmail.com>
2024-08-30 18:14:24 +0530

    feat: :tada: memento-app

---
+++ src/App.jsx
@@ -0,0 +1,93 @@
+import PWABadge from "./PWABadge.jsx";
+import shuffle from "./utilities/shuffle.js";
+import "./App.css";
+import { useState, useEffect } from "react";
+import Card from "./components/Card.jsx";
+import Header from "./components/Header.jsx";
+
+function App() {
+  const [wins, setWins] = useState(0);
+  const [cards, setCards] = useState(shuffle);
+  const [pickOne, setPickOne] = useState(null);
+  const [pickTwo, setPickTwo] = useState(null);
+  const [disabled, setDisabled] = useState(false);
+
+  const handleClick = (card) => {
+    if (!disabled) {
+      pickOne ? setPickTwo(card) : setPickOne(card);
+    }
+  };
+
+  const handleTurn = () => {
+    setPickOne(null);
+    setPickTwo(null);
+    setDisabled(false);
+  };
+
+  const handleNewGame = () => {
+    setWins(0);
+    handleTurn();
+    setCards(shuffle);
+  };
+
+  useEffect(() => {
+    let pickTimer;
+    if (pickOne && pickTwo) {
+      if (pickOne.image === pickTwo.image) {
+        setCards((prevCards) => {
+          return prevCards.map((card) => {
+            if (card.image === pickOne.image) {
+              return { ...card, matched: true };
+            } else {
+              return card;
+            }
+          });
+        });
+        handleTurn();
+      } else {
+        setDisabled(true);
+        pickTimer = setTimeout(() => {
+          handleTurn();
+        }, 1000);
+      }
+    }
+
+    return () => {
+      clearTimeout(pickTimer);
+    };
+  }, [cards, pickOne, pickTwo]);
+
+  useEffect(() => {
+    const checkWin = cards.filter((card) => !card.matched);
+
+    if (cards.length && checkWin.length < 1) {
+      console.log("You win!");
+      setWins(wins + 1);
+      handleTurn();
+      setCards(shuffle);
+    }
+  }, [cards, wins]);
+
+  return (
+    <>
+      <Header handleNewGame={handleNewGame} wins={wins} />
+      <div className="grid">
+        {cards.map((card) => {
+          const { image, id, matched } = card;
+
+          return (
+            <Card
+              key={id}
+              image={image}
+              onClick={() => handleClick(card)}
+              selected={card === pickOne || card === pickTwo || matched}
+            />
+          );
+        })}
+      </div>
+      <PWABadge />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Card.jsx
@@ -0,0 +1,17 @@
+const Card = ({ image, selected, onClick }) => {
+  return (
+    <div className="card">
+      <div className={selected ? "selected" : undefined}>
+        <img src={image} alt="" className="card-face" />
+        <img
+          src={"/assets/fireship.png"}
+          alt=""
+          className="card-back"
+          onClick={onClick}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Card;
